a09140aa0ee0b3dff1d04db5a1942dd7849d4b8f
Adam Kaewell <adam.kaewell@gmail.com>
2020-06-30 20:52:54 -0400

    created new user broadcast event, geolocation event with callback, implemented bad-words check for message event

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
+const Filter = require('bad-words')
 
 const app = express()
 const server = http.createServer(app)
@@ -16,9 +17,27 @@ io.on('connection', (socket) => {
   console.log('New WebSocket Connection')
 
   socket.emit('message', "Welcome!")
+  socket.broadcast.emit('message', 'A new user has joined!')
 
-  socket.on('sendMessage', (msg) => {
+  socket.on('sendMessage', (msg, callback) => {
+    const filter = new Filter()
+
+    if (filter.isProfane(msg)) {
+      return callback('Profanity is not allowed.')
+    }
+
     io.emit('message', msg)
+    callback()
+  })
+
+  socket.on('sendLocation', (position, callback) => {
+    io.emit('message', `https://google.com/maps?q=${position.latitude},${position.longitude}`)
+    
+    callback()
+  })
+
+  socket.on('disconnect', () => {
+    io.emit('message', 'A user has left!')
   })
 })
 
